c8d17a7f468f47a72d0a56e2945bfeffc55fd45c
rfl-urbaniak <rfl.urbaniak@gmail.com>
2022-11-13 08:46:09 +0100

    done with calculations for false positives

--- imprecision_weight/scripts/falsePositivesHOP.R
+++ imprecision_weight/scripts/falsePositivesHOP.R
@@ -1,14 +1,15 @@
-install.packages(c("coda","mvtnorm","devtools","loo","dagitty","shape"))
-devtools::install_github("rmcelreath/rethinking")
+#install.packages(c("coda","mvtnorm","devtools","loo","dagitty","shape"))
+#devtools::install_github("rmcelreath/rethinking")
 # we recommend running this is a fresh R session or restarting your current session
-install.packages("cmdstanr", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
-
-
+#install.packages("cmdstanr", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
 
 
 library(ggplot2)
 library(rethinking)
 library(truncnorm)
+library(ggthemes)
+library(reshape2)
+library(gridExtra)
 
 # In addition---and more importantly---equating the denominator $\pr{\textsf{match} \vert \neg \textsf{source}}$ with the random match probability ignores the risk of false positive matches. This risk is not negligible [@Shaer2016False]. The denominator, in fact, should depend on two sources of error: a false positive match and a coincidental match. These errors are quite distinct. For suppose two individuals---say the perpetrator and the defendant---happen to share the same DNA profile by coincidence. If an expert states that the crime scene sample and the defendant's sample match, this would be a coincidental match, not a false positive match. This risk of error is captured by the random match probability. But if the two samples do not actually match, and yet the expert says that they do, this would count as a false positive match, not a coincidental match. This risk of error is not captured by the random match probability.
 
@@ -26,6 +27,7 @@ fpdN <- fpdN/sum(fpdN)
 fpdNsample <- sample(ps, 1e4, replace = TRUE, prob = fpdN)
 
 ggplot()+geom_line(aes(x = ps, y = fpdN))+xlim(0,.02)
+
 
 ggplot()+geom_density(aes(x = fpdNsample))+xlim(0,.02)
 
@@ -48,15 +50,8 @@ mean(fpdWsample <= .01)
 
 max(fpdWsample)
 
-rmp <- 10e-9
-
-
-
 
 prior <- seq(0,.3, by = 0.001)
-priorH0 <- 1-prior
-
-#POSTERIOR IS
 posterior <- function(prior, rmp = 10e-9, fpp){ 
   return ( prior/(prior + rmp + fpp) )
 }
@@ -72,125 +67,63 @@ for (s in 1:1e4){
   fppN <-    fpdNsample[s] 
   fppW <-    fpdWsample[s] 
   
-  posteriorN[[s]] <- posterior(prior, fpp = fpdNsample[s])
-  posteriorW[[s]] <- posterior(prior, fpp = fpdWsample[s])
-  
-  minimaN[s] <- min(prior[posteriorN[[s]] > .99])
-  minimaW[s] <- min(prior[posteriorW[[s]] > .99])
+  posteriorN[[s]] <- posterior(prior = prior, fpp = fpdNsample[s])
+  posteriorW[[s]] <- posterior(prior = prior, fpp = fpdWsample[s])
+
+minimaN[s] <- ifelse(sum(posteriorN[[s]]> .99) >0, min(prior[posteriorN[[s]] > .99]), 1)
+minimaW[s] <- ifelse(sum(posteriorW[[s]]> .99) >0, min(prior[posteriorW[[s]] > .99]), 1)
 }
+
+
 posteriorNDF <-   do.call(cbind, posteriorN)
 posteriorWDF <-   do.call(cbind, posteriorW)
 
+str(posteriorNDF)
+str(prior)
 
-minimaPlot <- ggplot()+geom_density(aes(x = minima))+
-  theme_tufte(base_size = 10)+ggtitle("Minimal priors sufficient for posterior >.99")+xlab("minimal prior")+
+
+minimaPlot <- ggplot()+geom_density(aes(x = minimaN))+
+  geom_density(aes(x = minimaW), color = "orangered")+
+  theme_tufte(base_size = 10)+ggtitle("Minimal priors sufficient for posterior >.99")+
+  xlab("minimal prior")+
   theme(plot.title.position = "plot")
 
-minimaGrob <- ggplotGrob(minimaPlot)
 
-alpha = .3
-size = .08
-densitiesLinesPlot <- ggplot()+geom_line(aes(x = prior, y = jointPosteriorDF[,1]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,2]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,3]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,4]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,5]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,6]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,7]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,8]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,9]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,10]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,11]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,12]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,13]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,14]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,15]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,16]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,17]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,18]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,19]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,20]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,21]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,22]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,23]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,24]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,25]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,26]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,27]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,28]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,29]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,30]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,31]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,32]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,33]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,34]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,35]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,36]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,37]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,38]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,39]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,40]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,41]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,42]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,43]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,44]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,45]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,46]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,47]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,48]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,49]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,50]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,51]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,52]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,53]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,54]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,55]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,56]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,57]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,58]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,59]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,60]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,61]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,62]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,63]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,64]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,65]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,66]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,67]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,68]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,69]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,70]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,71]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,72]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,73]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,74]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,75]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,76]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,77]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,78]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,79]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,80]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,81]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,82]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,83]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,84]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,85]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,86]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,87]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,88]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,89]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,90]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,91]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,92]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,93]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,94]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,95]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,96]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,97]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,98]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,99]), alpha = alpha, size = size)+
-  geom_line(aes(x = prior, y = jointPosteriorDF[,100]), alpha = alpha, size = size)+
-  xlim(0,.1)+theme_tufte(base_size = 10)+ylab("posterior")+
+
+minimaPlot
+
+minimaGrob <- ggplotGrob(minimaNPlot)
+
+posteriorNDFsubset <- as.data.frame(posteriorNDF[,1:100])
+posteriorNDFsubset$prior <- prior
+posteriorNDFsubsetLong <- melt(posteriorNDFsubset, id.vars = "prior")
+
+
+posteriorWDFsubset <- as.data.frame(posteriorWDF[,1:100])
+posteriorWDFsubset$prior <- prior
+posteriorWDFsubsetLong <- melt(posteriorWDFsubset, id.vars = "prior")
+
+
+
+
+alpha = .7
+size = .1
+
+posteriorNplot <- ggplot(posteriorNDFsubsetLong, aes(x = prior, y = value, group = variable))+
+  geom_line(alpha = alpha, size = size)+
+  theme_tufte(base_size = 10)+ylab("posterior")
+
+
+posteriorWplot <- ggplot(posteriorWDFsubsetLong, aes(x = prior, y = value, group = variable))+
+  geom_line(alpha = alpha, size = size)+
+  theme_tufte(base_size = 10)+ylab("posterior")
+
+
+
+grid.arrange(posteriorNplot, posteriorWplot)
+
+
++
   annotation_custom(minimaGrob, xmin = .025, xmax = .1, ymin = 0.01, ymax = 0.8)+
   ggtitle("Posterior vs prior (100 sampled lines)")+
   theme(plot.title.position = "plot")
